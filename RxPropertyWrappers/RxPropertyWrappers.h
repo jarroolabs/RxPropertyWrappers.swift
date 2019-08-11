884bc9312f34118e2d9f596c3719a16c5c41c43c
Alex Manarpies <alex@jarroo.com>
2019-08-11 16:35:58 +0200

    Initial Commit

---
+++ RxPropertyWrappers/RxPropertyWrappers.h
@@ -0,0 +1,19 @@
+//
+//  RxPropertyWrappers.h
+//  RxPropertyWrappers
+//
+//  Created by Alex Manarpies on 11/08/2019.
+//  Copyright © 2019 Jarroo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxPropertyWrappers.
+FOUNDATION_EXPORT double RxPropertyWrappersVersionNumber;
+
+//! Project version string for RxPropertyWrappers.
+FOUNDATION_EXPORT const unsigned char RxPropertyWrappersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxPropertyWrappers/PublicHeader.h>
+
+
